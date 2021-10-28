7fc71f02bb27c2d4cc4f6daa6871309542a664be
ridgeroman <ridgeroman.st@gmail.com>
2021-10-27 21:27:52 -0400

    contains updated bin and src

--- src/gitProject/git01.java
+++ src/gitProject/git01.java
@@ -4,7 +4,9 @@ public class git01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello java11");
+		
+		System.out.println("rose are blue");
 	}
 
 }
